6073c09daeb4503f3b003641799e7b2ceebd4584
TranHaiz <tranhai002456@gmail.com>
2026-06-06 10:14:28 +0700

    update: Relasae version 1.1.3

--- lib/common/device_config.h
+++ lib/common/device_config.h
@@ -19,7 +19,7 @@
 // ------------------------------ Device configuration ------------------------------
 #define FIRRMWARE_MAJOR_VERSION         (1)
 #define FIRRMWARE_MINOR_VERSION         (1)
-#define FIRRMWARE_PATCH_VERSION         (2)
+#define FIRRMWARE_PATCH_VERSION         (3)
 
 #define DEFAULT_DEVICE_NAME             "haq-trk-000"
 #define DEVICE_NAME_MAX_LEN             (32)
